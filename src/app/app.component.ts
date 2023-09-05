652d07f8be8dc6fbecb082c44f77c63fe61f0c6f
19church <saint2737@gmail.com>
2023-09-05 21:56:54 +0700

    okok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  id: any;
+  name: any;
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api/resource').subscribe(data => {
+      this.id = data[0].id;
+      this.name = data[0].name;
+    })
+  }
   title = 'lab1-angular-app';
 }
